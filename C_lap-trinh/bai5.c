7648cf07f0fa9b492bcae319442b79fe67ce257d
e421j <ptiendung2021@gmail.com>
2026-05-17 21:22:08 +0700

    them bai 5

---
+++ C_lap-trinh/bai5.c
@@ -0,0 +1,15 @@
+// Gán giá trị người dùng
+// Xác định giá trị người dùng
+// k\n: là cấp phát dữ liệu để sử dụng dữ liệu trong khu đó.Chỗ chứu dữ liệu còn đc gội là hộp
+#include <stdio.h>
+int main() {
+    int age;
+    float gpa;
+    char lop;
+    char name[]= "Tien Dung";// Hoặc là sử dụng name[30]=> xác định giới hạn kí tự để gõ
+    printf("%d\n", age);
+    printf("%f\n", gpa);
+    printf("%c\n", lop);
+    printf("%s\n", name);
+    return 0;
+}
