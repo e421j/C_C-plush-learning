bb92e04a2da16dfc704cea9704ff3bee750e68d7
e421j <ptiendung2021@gmail.com>
2026-05-19 22:29:01 +0700

    sua bai 5 va add bai 5-2

---
+++ C_lap-trinh/bai5-2.c
@@ -0,0 +1,14 @@
+// underfined behavior ( hành vi không xác định)
+//là sự ngẫu nhiên về giá trị khi in ra màn hình và các giá trị ngẫu nhiên đó đc lấy từ trong hộp hay còn đc gọi là giá trị rác
+#include <stdio.h>
+int main() {
+    int age;
+    float gpa;
+    char lop;
+    char name[30];
+    printf("%d\n", age);
+    printf("%f\n", gpa);
+    printf("%c\n", lop);
+    printf("%s\n", name);
+    return 0;
+}
